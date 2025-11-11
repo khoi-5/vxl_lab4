76c0273fd6d6103693c2fae26617314322ca4109
khoi-5 <tranminhkhoichaua5@gmail.com>
2025-11-11 23:48:26 +0700

    lab3

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -95,10 +95,9 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 	SCH_Init();
-	SCH_Add_Task(blinky_led2, 0, 2000);
-	SCH_Add_Task(blinky_led3, 10000, 0);
-	SCH_Add_Task(blinky_led4, 1000, 3000);
-	SCH_Add_Task(blinky_led5, 0, 2000);
+	SCH_Add_Task(fsm_auto_run, 0, 10);
+	SCH_Add_Task(fsm_config_run, 5000, 10);
+	SCH_Add_Task(fsm_manual_run, 5000, 10);
   while (1)
   {
     /* USER CODE END WHILE */
